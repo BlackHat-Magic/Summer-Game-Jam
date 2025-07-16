fec562061c2a0e97e75e2943c3bd498307537634
Luke Henderson <personal@lukeh.us>
2025-07-17 02:15:18 +0900

    followed clear tutorial on SDL3 page

---
+++ main.c
@@ -0,0 +1,54 @@
+#define SDL_MAIN_USE_CALLBACKS 1 // use callbacks instead of main
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+// runs at startup
+SDL_AppResult SDL_AppInit (void** appstate, int argc, char* argv[]) {
+    SDL_SetAppMetadata ("Uh", "1.0", "xyz.lukeh.summer-game-jam-2025");
+
+    if (!SDL_Init (SDL_INIT_VIDEO)) {
+        SDL_Log ("Couldn't initialize SDL: %s", SDL_GetError ());
+        return (SDL_APP_FAILURE);
+    }
+    if (!SDL_CreateWindowAndRenderer ("Uh", 640, 480, 0, &window, &renderer)) {
+        SDL_Log ("Couldn't create window/renderer: %s", SDL_GetError ());
+        return (SDL_APP_FAILURE);
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+// runs when a new event (mosue input, keypress, etc.) happens
+SDL_AppResult SDL_AppEvent (void* appstate, SDL_Event* event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return (SDL_APP_SUCCESS);
+    }
+
+    return (SDL_APP_CONTINUE);
+}
+
+// runs once each frame
+SDL_AppResult SDL_AppIterate (void* appstate) {
+    // time elapsed seconds
+    const double now = ((double) SDL_GetTicks ()) / 1000.0;
+
+    const float red     = (float) (0.5 + 0.5 * SDL_sin (now));
+    const float green   = (float) (0.5 + 0.5 * SDL_sin (now + SDL_PI_D * 2 / 3));
+    const float blue    = (float) (0.5 + 0.5 * SDL_sin (now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat (renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+
+    SDL_RenderClear (renderer);
+
+    // put the newly-cleared renderer on the screen
+    SDL_RenderPresent (renderer);
+
+    return (SDL_APP_CONTINUE);
+}
+
+void SDL_AppQuit (void* appstate, SDL_AppResult result) {
+    // ...
+}
